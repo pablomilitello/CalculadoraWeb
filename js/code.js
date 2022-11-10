9793a35b878a34a504b15c73dd17da00a591e624
Pablo Militello <pmilitelloaveni@gmail.com>
2022-11-10 11:24:45 -0300

    estoy probando

--- js/code.js
+++ js/code.js
@@ -1,93 +1,28 @@
-//Declaracion de variables
-let operacion;
+//Declaro variables
 let num1;
 let num2;
-let resultado;
+let numero;
+let operacion;
 
-while (
-  isNaN(operacion) ||
-  operacion == null ||
-  operacion == "" ||
-  operacion > 4 ||
-  operacion < 0
-) {
-  //Ingresar operacion
-  ingresarOperacion();
-  if (operacion > 4 || operacion < 1) {
-    alert("No se ha identificado la operación que desea realizar");
-  }
-}
+//Vinculo el DOM
+const btnNumero = document.querySelectorAll(".btnNumero");
+const btnOperador = document.querySelectorAll(".btnOperador");
+const borrarNumero = document.getElementById("borrarAnterior");
+const borrarTodo = document.getElementById("borrarTodo");
+const mostrarValorAnterior = document.getElementById("valorAnterior");
+const mostrarValorActual = document.getElementById("valorActual");
 
-//Verifica si es o no un numero
-while (
-  isNaN(num1) ||
-  num1 == null ||
-  num1 == "" ||
-  isNaN(num2) ||
-  num2 == null ||
-  num2 == ""
-) {
-  ingresarOperandos();
-}
+//Instancio el Visor
+const visor = new Visor(mostrarValorAnterior, mostrarValorActual);
 
-//Funcion ingresar operacion
-function ingresarOperacion() {
-  operacion = parseInt(
-    prompt(
-      "¿Qué operación deseas realizar?. Presione:\n 1 para Sumar \n 2 para Restar\n 3 para Multiplicar\n 4 para Dividir"
-    )
-  );
-}
+btnNumero.forEach((boton) => {
+  boton.addEventListener("click", () => visor.agregarNumero(boton.innerHTML));
+});
 
-//Funcion ingresar operandos
-function ingresarOperandos() {
-  num1 = parseFloat(
-    prompt("Ingrese el primer número con el que desea operar:")
-  );
-  num2 = parseFloat(
-    prompt("Ingrese el segundo número con el que desea operar:")
-  );
-}
+borrarNumero.addEventListener("click", () => visor.borrarAnterior());
+borrarTodo.addEventListener("click", () => visor.borrarVisor());
 
-class Calculadora {
-  constructor(num1, num2) {
-    this.num1 = num1;
-    this.num2 = num2;
-  }
-  sumar() {
-    return this.num1 + this.num2;
-  }
-  restar() {
-    return this.num1 - this.num2;
-  }
-  multiplicar() {
-    return this.num1 * this.num2;
-  }
-  dividir() {
-    return this.num1 / this.num2;
-  }
-}
-
-//Declaracion de funcion mostrarResultado
-const mostrarResultado = (resultado) => alert(resultado);
-
-//Switch Operaciones
-switch (operacion) {
-  case 1:
-    calculo1 = new Calculadora(num1, num2);
-    mostrarResultado(calculo1.sumar());
-    break;
-  case 2:
-    calculo2 = new Calculadora(num1, num2);
-    mostrarResultado(calculo2.restar());
-    break;
-  case 3:
-    calculo3 = new Calculadora(num1, num2);
-    mostrarResultado(calculo3.multiplicar());
-    break;
-    break;
-  case 4:
-    calculo4 = new Calculadora(num1, num2);
-    mostrarResultado(calculo4.dividir());
-    break;
-}
+btnOperador.forEach((boton) => {
+  boton.addEventListener("click", () => visor.computar(boton.value));
+  console.log(boton.value);
+});
