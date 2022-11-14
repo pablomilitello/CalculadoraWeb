efee5a8ee0725dd992f852e0b68deb5f6dcac4aa
Pablo Militello <pmilitelloaveni@gmail.com>
2022-11-14 16:58:47 -0300

    avanzando con DOM

--- js/Visor.js
+++ js/Visor.js
@@ -4,15 +4,56 @@ class Visor {
   constructor(visorValorAnterior, visorValorActual) {
     this.visorValorAnterior = visorValorAnterior;
     this.visorValorActual = visorValorActual;
+    this.valorAnterior = "";
+    this.valorActual = "";
+    this.calculadora = new Calculadora();
+    this.operacion = undefined;
+    this.signos = {
+      sumar: "+",
+      restar: "-",
+      multiplicar: "x",
+      dividir: "÷",
+    };
   }
 
   agregarOperando(operando) {
-    this.visorValorActual = this.visorValorActual + operando;
+    if (operando == "." && this.valorActual.includes(".")) return;
+    else {
+      this.valorActual = this.valorActual.toString() + operando.toString();
+      this.imprimirEnVisor();
+    }
+  }
+
+  agregarOperaciones(operaciones) {
+    this.tipoOperacion != "igual" && this.operar();
+    this.operacion = operaciones;
+    this.valorAnterior = this.valorActual || this.valorAnterior;
+    this.valorActual = "";
+    this.imprimirEnVisor();
+  }
+
+  borrarTodo() {
+    this.valorActual = "";
+    this.valorAnterior = "";
+    this.operacion = undefined;
+    this.imprimirEnVisor();
+  }
+
+  borrarAnterior() {
+    this.valorActual = this.valorActual.slice(0, -2);
     this.imprimirEnVisor();
   }
 
   imprimirEnVisor() {
-    this.visorValorActual.textContent = parseFloat(this.visorValorActual);
-    this.visorValorAnterior.textContent = this.visorValorAnterior;
+    this.visorValorActual.textContent = this.valorActual;
+    this.visorValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.operacion] || ""}`;
+  }
+
+  operar() {
+    const valorActual = parseFloat(this.valorActual);
+    const valorAnterior = parseFloat(this.valorAnterior);
+
+    if (isNaN(valorActual) || isNaN(valorAnterior)) return;
+    this.valorActual = this.calculadora[this.operacion](valorAnterior, valorActual);
   }
 }
--- js/code.js
+++ js/code.js
@@ -7,11 +7,24 @@ const btnNumero = document.querySelectorAll(".btnNumero");
 const btnOperador = document.querySelectorAll(".btnOperador");
 const visorValorActual = document.getElementById("valorActual");
 const visorValorAnterior = document.getElementById("valorAnterior");
+const btnBorrarTodo = document.getElementById("borrarTodo");
+const btnBorrarAnterior = document.getElementById("borrarAnterior");
 
 //Instanciar Visor
 const visor = new Visor(visorValorAnterior, visorValorActual);
 
-//Escuchar la botonera
+//Escuchar botones de operandos
 btnNumero.forEach((btn) => {
   btn.addEventListener("click", () => visor.agregarOperando(btn.innerHTML));
 });
+
+//Escuchar botones de operadores
+btnOperador.forEach((btn) => {
+  btn.addEventListener("click", () => visor.agregarOperaciones(btn.value, btn.innerHTML));
+});
+
+//Escuchar boton borrar todo
+btnBorrarTodo.addEventListener("click", () => visor.borrarTodo());
+
+//Escuchar boton borrar anterior
+btnBorrarAnterior.addEventListener("click", () => visor.borrarAnterior());
